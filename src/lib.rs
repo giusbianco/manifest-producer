db53e2ce5c302c3600bd4abe6d836a88d5818c21
giusbianco <white951@hotmail.it>
2024-02-01 18:37:58 +0100

    add json
    Creation of the json file with expected api and binary features added.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,29 @@
 use goblin::elf::Elf;
+use std::fs::File;
 use std::process::Command;
-use std::io::{BufReader, BufRead};
+use std::io::{BufRead, BufReader, Write};
+use std::vec;
+use serde::Serialize;
+use std::collections::HashSet;
 
 mod error;
-use error::Result;
+use error::{Result, Error};
+
+#[derive(Debug, Serialize)]
+struct Manifest {
+    architecture: String,
+    stripped: bool,
+    api_found: Option<Vec<String>>,
+    syscall_categories: Option<SyscallFeatures>,
+}
+
+#[derive(Debug, Serialize)]
+struct SyscallFeatures {
+    network: Option<HashSet<String>>,
+    device: Option<HashSet<String>>,
+    disk: Option<HashSet<String>>,
+    memory:Option<HashSet<String>>,
+}
 
 pub fn elf_analysis(file_path: &str) -> Result<()> {
     // Load the ELF file
@@ -14,14 +34,37 @@ pub fn elf_analysis(file_path: &str) -> Result<()> {
 
     // Retrieve the architecture from the ELF header
     let architecture = arch_recovery(&elf);
-    println!("Architecture: {}", architecture);
-    // TODO: handle return if 'unknown'
-
-    if !is_stripped(&elf) {
-        api_search(&elf)
+    if architecture == "Unknown" {
+        return Err(Error::InvalidElf {
+            source: goblin::error::Error::Malformed("Unsupported architecture".to_string()),
+        });
     }
 
-    syscall_tracing(file_path);
+    let stripped = is_stripped(&elf);
+
+    // Initialize the manifest
+    let mut manifest = Manifest {
+        architecture: architecture.to_string(),
+        stripped,
+        api_found: None,
+        syscall_categories: None,
+    };
+
+    // If not stripped, search for APIs
+    if !stripped {
+        manifest.api_found = Some(api_search(&elf));
+    }
+
+    // Dynamic analysis with strace
+    let syscall_categories = syscall_tracing(file_path);
+    manifest.syscall_categories = Some(syscall_categories?);
+
+    // Serialize the manifest to JSON and print it
+    let manifest_json = serde_json::to_string_pretty(&manifest).unwrap();
+
+    let json_file_path = "./manifest.json";
+    let mut file = File::create(json_file_path)?;
+    file.write_all(manifest_json.as_bytes())?;
 
     Ok(())
 }
@@ -51,7 +94,6 @@ fn is_stripped(elf: &Elf) -> bool {
     }
 }
 
-
 // Check for the presence of the specified sections
 fn has_sections(elf: &Elf, section_type: u32) -> bool {
     elf.section_headers
@@ -60,20 +102,20 @@ fn has_sections(elf: &Elf, section_type: u32) -> bool {
 }
 
 // Function to search for APIs in the symbol table
-fn api_search(elf: &Elf) {
+fn api_search(elf: &Elf) -> Vec<String> {
     let api_list = vec!["turnLampOn", "turnLampOff"];
+    let mut api_found = Vec::new();
 
     for symbol in elf.syms.iter() {
-        if symbol.st_type() == goblin::elf::sym::STT_FUNC 
-        && symbol.st_shndx != 0 
-        {
+        if symbol.st_type() == goblin::elf::sym::STT_FUNC && symbol.st_shndx != 0 {
             if let Some(function_name) = get_function_name(&elf, &symbol) {
                 if api_list.contains(&function_name) {
-                    println!("API [{}] found!", function_name);
+                    api_found.push(function_name.to_string());
                 }
             }
         }
     }
+    api_found
 }
 
 fn get_function_name<'a>(elf: &'a Elf, symbol: &'a goblin::elf::Sym) -> Option<&'a str> {
@@ -86,39 +128,68 @@ fn get_function_name<'a>(elf: &'a Elf, symbol: &'a goblin::elf::Sym) -> Option<&
     return Some(name_str);
 }
 
-fn syscall_tracing(binary_path: &str) {
+fn syscall_tracing(binary_path: &str) -> Result<SyscallFeatures> {
 
-    let output = Command::new("strace")
+    let _output = Command::new("strace")
     .arg("-o")
     .arg("./binaries/strace_output.txt")
     .arg(binary_path)
-    .output()
-    .expect("Failed to execute strace");
+    .output()?;
 
-    if !output.status.success() {
-        println!("Error executing strace");
-        return;
-    }
-
-    let file = std::fs::File::open("./binaries/strace_output.txt").expect("Failed to open strace output file");
+    let file = std::fs::File::open("./binaries/strace_output.txt")?;
     let reader = BufReader::new(file);
+    let mut syscall_categories = SyscallFeatures {
+        network: Some(HashSet::new()),
+        device: Some(HashSet::new()),
+        disk: Some(HashSet::new()),
+        memory: Some(HashSet::new()),
+    };
 
     for line in reader.lines() {
         if let Ok(line) = line {
-            if let Some(category) = categorize_syscall(&line) {
-                println!("Categoria: {},", category);
+            if let Some((category, syscall)) = categorize_syscall(&line) {
+                match category {
+                    "network" => {
+                        if let Some(network_category) = &mut syscall_categories.network {
+                            network_category.insert(syscall.to_string());
+                        }
+                    }
+                    "device" => {
+                        if let Some(device_category) = &mut syscall_categories.device {
+                            device_category.insert(syscall.to_string());
+                        }
+                    }
+                    "disk" => {
+                        if let Some(disk_category) = &mut syscall_categories.disk {
+                            disk_category.insert(syscall.to_string());
+                        }
+                    }
+                    "memory" => {
+                        if let Some(mem_category) = &mut syscall_categories.memory {
+                            mem_category.insert(syscall.to_string());
+                        }
+                    }
+                    _ => {}
+                }
             }
         }
     }
+    Ok(syscall_categories)
 }
 
-fn categorize_syscall(syscall: &str) -> Option<&str> {
+fn categorize_syscall<'a>(syscall: &'a str) -> Option<(&'a str, &'a str)> {
     if syscall.contains("write") {
-        Some("Scrittura su disco")
-    } else if syscall.contains("connect") || syscall.contains("sendto") || syscall.contains("recvfrom") {
-        Some("Accesso alla rete")
-    } else if syscall.contains("ioctl") || syscall.contains("read") {
-        Some("Accesso ai driver di dispositivo")
+        Some(("disk", "write_disk"))
+    } else if syscall.contains("connect") {
+        Some(("network", "connection_attempt"))
+    } else if syscall.contains("recvfrom") {
+        Some(("network", "recv_data"))
+    } else if syscall.contains("sendto") {
+        Some(("network", "send_data"))
+    } else if syscall.contains("ioctl") {
+        Some(("device", "I/O control"))
+    } else if syscall.contains("mmap") {
+        Some(("memory", "access_memory"))
     } else {
         None
     }
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@ use manifest_producer::elf_analysis;
 mod error;
 
 fn main() {
-    let elf_file_path = "./binaries/debug-info/fake-firmware-c";
-    //let elf_file_path = "./binaries/stripped/fake-firmware-c";
+    //let elf_file_path = "./binaries/debug-info/fake-firmware-c";
+    let elf_file_path = "./binaries/stripped/fake-firmware";
 
     match elf_analysis(elf_file_path) {
-        Ok(_elf) => {
-
+        Ok(_) => {
+            println!("Manifest created.");
+            println!("End of binary analysis.");
         }
         Err(error) => {
             eprintln!("Error: {}", error);
